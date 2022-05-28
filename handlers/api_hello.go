60b66fb03d8255152da0a67720bbdd2952c4b957
Peter Svehla <>
2022-05-28 22:34:18 +1000

    Tidy.

--- handlers/api_hello.go
+++ handlers/api_hello.go
@@ -1,21 +1,22 @@
 package handlers
+
 import (
-    "github.com/GIT_USER_ID/GIT_REPO_ID/models"
-    "github.com/labstack/echo/v4"
-    "net/http"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/psvehla/hello-echo/models"
 )
 
 // HelloHello - hello hello
 func (c *Container) HelloHello(ctx echo.Context) error {
-    return ctx.JSON(http.StatusOK, models.HelloWorld {
-        Message: "Hello World",
-    })
+	return ctx.JSON(http.StatusOK, models.HelloWorld{
+		Message: "Hello World",
+	})
 }
-
 
 // HelloOpenapiJson - Download ./gen/http/openapi.json
 func (c *Container) HelloOpenapiJson(ctx echo.Context) error {
-    return ctx.JSON(http.StatusOK, models.HelloWorld {
-        Message: "Hello World",
-    })
+	return ctx.JSON(http.StatusOK, models.HelloWorld{
+		Message: "Hello World",
+	})
 }
